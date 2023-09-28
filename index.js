f079f4048aac10a9aa7a31b384ae15b75dc90e02
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-09-28 13:01:14 +0300

    Tests are done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+let spy;
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+let count = 0;
+function writeCards(){
+    let thankMessage = [];
+    for(count ; count < names.length ; count++){
+        thankMessage.push(`Thank you, ${names[count]}, for the wonderful surprise gift!`);
+    }
+    return thankMessage;
+}
+
+
+function countDown(){
+    let number = 10;
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+}
